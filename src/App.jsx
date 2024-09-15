e3d2a7d8b0e66d587f2c8ffe4a4bca41b52ebf1e
naquio <ignaciocanal@live.com.ar>
2024-09-15 11:14:00 -0300

    empezando nuevo curso

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Title from './componentes/Title'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Primera Prueba</h1>
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Title.jsx
@@ -0,0 +1,7 @@
+const Title = () =>{
+  return(
+    <h2>Final de la Clase 2</h2>
+  )
+}
+
+export default Title
